009551a48d10092d178546083ad59e4cd7c50a5b
vinzeebreak <vincenthoulbreque@gmail.com>
2018-03-20 19:14:38 +0100

    added chrome installation

---
+++ ubuntu_1604/chrome.sh
@@ -0,0 +1,7 @@
+echo "Installation of google chrome for 64 bits"
+
+cd ~
+
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome-stable_current_amd64.deb
+sudo apt-get install -f
