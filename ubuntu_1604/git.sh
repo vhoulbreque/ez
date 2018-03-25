7d21155a9b1854847c84c7355596518c385196f0
vinzeebreak <vincenthoulbreque@gmail.com>
2018-03-26 00:02:14 +0200

    git was not at the last version. Now, ensures it is

--- ubuntu_1604/git.sh
+++ ubuntu_1604/git.sh
@@ -2,7 +2,9 @@ echo "Installation of git"
 
 cd ~
 
-sudo apt-get install git
+sudo add-apt-repository ppa:git-core/ppa -y
+sudo apt-get update
+sudo apt-get install git -y
 
 git config --global user.email "vincenthoulbreque@gmail.com"
 git config --global user.name "vinzeebreak"
