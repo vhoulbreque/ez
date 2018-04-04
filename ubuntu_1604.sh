a6e96b19ffa8d35013cc93aab6ea828d3d12ec31
vinzeebreak <vincenthoulbreque@gmail.com>
2018-04-04 11:50:29 +0200

    all scripts are executable. No need to keep chmod +x in main

--- ubuntu_1604.sh
+++ ubuntu_1604.sh
@@ -9,43 +9,33 @@ sudo apt-get update
 sudo apt-get upgrade
 
 # Common tools
-chmod +x ubuntu_1604/common-tools.sh
 ./ubuntu_1604/common-tools.sh
 
 # Git
-chmod +x ubuntu_1604/git.sh
 ./ubuntu_1604/git.sh
 
 # Mysql-server
-chmod +x ubuntu_1604/mysql-server.sh
 ./ubuntu_1604/mysql-server.sh
 
 # MongoDB
-chmod +x ubuntu_1604/mongodb.sh
 ./ubuntu_1604/mongodb.sh
 
 # Anaconda
-chmod +x ubuntu_1604/anaconda.sh
 ./ubuntu_1604/anaconda.sh
 
 # Spotify
-chmod +x ubuntu_1604/spotify.sh
 ./ubuntu_1604/spotify.sh
 
 # Atom
-chmod +x ubuntu_1604/atom.sh
 ./ubuntu_1604/atom.sh
 
 # Nteract
-chmod +x ubuntu_1604/nteract.sh
 ./ubuntu_1604/nteract.sh
 
 # Skype
-chmod +x ubuntu_1604/skype.sh
 ./ubuntu_1604/skype.sh
 
 # fastText
-chmod +x ubuntu_1604/fasttext.sh
 ./ubuntu_1604/fasttext.sh
 
 echo "clean-up"
