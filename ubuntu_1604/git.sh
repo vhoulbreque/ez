9421f493ed82ce4caa5be187442bf54906b0f064
vinzeebreak <vincenthoulbreque@gmail.com>
2018-04-04 11:37:15 +0200

    removed cloning of repositories

--- ubuntu_1604/git.sh
+++ ubuntu_1604/git.sh
@@ -8,14 +8,3 @@ sudo apt-get install git -y
 
 git config --global user.email "vincenthoulbreque@gmail.com"
 git config --global user.name "vinzeebreak"
-
-echo "Clone repositories"
-
-mkdir Projects
-cd Projects
-git clone https://github.com/vinzeebreak/ironcar
-git clone https://github.com/vinzeebreak/road_simulator
-git clone https://github.com/vinzeebreak/lpr.git
-mkdir forks
-cd forks
-git clone https://github.com/Hugoo/ironcar.git hugo
