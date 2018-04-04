278d1c13206ef3509820024bf9730c2fa1171d1f
vinzeebreak <vincenthoulbreque@gmail.com>
2018-04-04 16:33:50 +0200

    chrome .deb is deleted after installation

--- ubuntu_1604/chrome.sh
+++ ubuntu_1604/chrome.sh
@@ -5,3 +5,4 @@ cd ~
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i google-chrome-stable_current_amd64.deb
 sudo apt-get install -f
+rm google-chrome-stable_current_amd64.deb
