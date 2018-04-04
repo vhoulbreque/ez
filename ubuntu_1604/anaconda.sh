d8739ce55c3e0bf02495853b3945afda909df6b6
vinzeebreak <vincenthoulbreque@gmail.com>
2018-04-04 11:48:24 +0200

    removed conda environments

--- ubuntu_1604/anaconda.sh
+++ ubuntu_1604/anaconda.sh
@@ -7,8 +7,4 @@ source ~/.bashrc
 conda list
 conda update -n base conda
 
-conda create --name lpr34 python=3.4 --yes
-conda create --name ironcarenv python=3.6 --yes
-conda create --name thalesenv python=3.6 --yes
-
 rm Anaconda3-5.1.0-Linux-x86_64.sh
