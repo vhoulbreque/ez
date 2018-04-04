63eff5e44e60dd906240c0cbabbf4455b931120d
vinzeebreak <vincenthoulbreque@gmail.com>
2018-04-04 11:35:57 +0200

    added atom packages installation

--- ubuntu_1604/atom.sh
+++ ubuntu_1604/atom.sh
@@ -5,3 +5,9 @@ cd ~
 wget https://atom.io/download/deb -O atom-deb
 sudo dpkg -i atom-deb
 rm atom-deb
+
+# Installation of atom packages
+
+apm install autocomplete-python
+apm install file-icons
+apm install python-autopep8
