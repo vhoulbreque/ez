3366c937bb2f40b95a7ee92e2e0d447e7702b571
vinzeebreak <vincenthoulbreque@gmail.com>
2018-04-07 16:10:17 +0200

    added jq for json parsing + git now reads config for email and name

--- README.md
+++ README.md
@@ -2,3 +2,12 @@
 
 This repository contains command-line installations of common tools.
 Currently, there is only Ubuntu 16.04
+
+The `config.json` file should look like this:
+
+```json
+{
+  "git_email": "the_email_of_my_account@example.com",
+  "git_name": "my_git_pseudo"
+}
+```
--- ubuntu_1604/common-tools.sh
+++ ubuntu_1604/common-tools.sh
@@ -2,5 +2,7 @@ echo "Installation of common tools"
 
 cd ~
 
-sudo apt-get install htop gnome-shell vlc -y
-sudo apt install ffmpeg
+sudo apt-get install htop gnome-shell vlc ffmpeg -y
+
+# Installation of jq for json parsing
+sudo apt-get install jq -y  # https://github.com/stedolan/jq
