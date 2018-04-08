5d310d5e8d64e7b92c7e8f0f3ddbb3e4ffdab513
vinzeebreak <vincenthoulbreque@gmail.com>
2018-04-08 20:27:45 +0200

    added miniconda install

---
+++ ubuntu_1604/miniconda.sh
@@ -0,0 +1,7 @@
+echo "Installation of miniconda"
+
+cd ~
+
+wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
+bash Miniconda3-latest-Linux-x86_64.sh
+rm Miniconda3-latest-Linux-x86_64.sh
