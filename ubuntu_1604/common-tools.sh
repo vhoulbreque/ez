38fa6da7739d390c5db20494066cff85f12d8149
vinzeebreak <vincenthoulbreque@gmail.com>
2018-04-08 20:54:28 +0200

    gcc, python-dev, python3-dev libevent-dev and build-essential added

--- ubuntu_1604/common-tools.sh
+++ ubuntu_1604/common-tools.sh
@@ -2,7 +2,12 @@ echo "Installation of common tools"
 
 cd ~
 
-sudo apt-get install htop gnome-shell vlc ffmpeg -y
+sudo apt-get install htop gnome-shell vlc ffmpeg gcc -y
+sudo apt-get install python-dev -y
+sudo apt-get install python3-dev -y
+sudo apt-get install libevent-dev -y
+sudo apt-get update
+sudo apt-get install --reinstall build-essential
 
 # Installation of jq for json parsing
 sudo apt-get install jq -y  # https://github.com/stedolan/jq
