e6a033ed9dae439f4c73a02066978a48cbb45d9f
vinzeebreak <vincenthoulbreque@gmail.com>
2018-04-14 09:35:34 +0200

    remove installation file for go

--- ubuntu_1604/go.sh
+++ ubuntu_1604/go.sh
@@ -4,6 +4,7 @@ cd ~
 sudo echo
 wget -O go_install.tar.gz https://dl.google.com/go/go1.10.1.linux-amd64.tar.gz
 sudo tar -C /usr/local -xzf go_install.tar.gz
+rm go_install.tar.gz
 
 # Adding the folder to the path
 cd ~
