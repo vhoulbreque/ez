dafed184f86d300b2cc6536800243cfe441d2079
vinzeebreak <vincenthoulbreque@gmail.com>
2018-04-15 00:34:40 +0200

    updated version of nteract

--- ubuntu_1604/nteract.sh
+++ ubuntu_1604/nteract.sh
@@ -3,6 +3,6 @@ echo "Installation of nteract"
 cd ~
 
 sudo apt-get install libappindicator1 libindicator7 libaudit-common libaudit1 -y
-wget https://github.com/nteract/nteract/releases/download/v0.7.1/nteract_0.7.1_amd64.deb
-sudo dpkg -i nteract_0.7.1_amd64.deb
-rm nteract_0.7.1_amd64.deb
+wget -O nteract_latest.deb https://github.com/nteract/nteract/releases/download/v0.8.4/nteract_0.8.4_amd64.deb
+sudo dpkg -i nteract_latest.deb
+rm nteract_latest.deb
