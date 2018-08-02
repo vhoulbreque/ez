17d967e83136052f5f5a1f2d9fb83775f51b9fca
vinzeebreak <vincenthoulbreque@gmail.com>
2018-08-02 23:02:18 +0200

    changed node version

--- ubuntu_1604/install/node.sh
+++ ubuntu_1604/install/node.sh
@@ -3,7 +3,7 @@ echo "Installation of node"
 cd ~
 
 sudo apt-get install build-essential -y
-curl -sL https://deb.nodesource.com/setup_9.x | sudo -E bash -
+curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
 sudo apt-get install nodejs -y
 
 echo "nodejs version : $(nodejs -v)"
