29aa18cbf223aad8a396e3733332bead40569c15
vinzeebreak <vincenthoulbreque@gmail.com>
2018-08-07 22:35:08 +0200

    added macos install script

---
+++ macos.sh
@@ -0,0 +1,10 @@
+echo "##################################"
+echo "# Welcome to the MacOS installer #"
+echo "##################################"
+
+DIR_SCRIPTS="./macos/install"
+
+# Common tools
+$DIR_SCRIPTS/common-tools.sh
+
+echo "The installation is over !"
