c7fee52d1a099708ce692ffc5865a4924a1c7435
vinzeebreak <vincenthoulbreque@gmail.com>
2018-08-27 18:36:31 +0200

    typo

--- scripts/hyper.sh
+++ scripts/hyper.sh
@@ -2,7 +2,7 @@ platform=$1  # linux-gnu, darwin*
 mode=$2  # install, uninstall
 
 if [[ $mode == "install" ]]; then
-  echo "Installation of Hugo"
+  echo "Installation of Hyper"
 
   if [[ $platform == "linux-gnu" ]]; then
     cd ~
@@ -15,11 +15,11 @@ if [[ $mode == "install" ]]; then
     exit 1
   fi
 
-  echo "Installation of Hugo complete"
+  echo "Installation of Hyper complete"
 
 elif [[ $mode == "uninstall" ]]; then
-  echo "Uninstallation of Hugo"
+  echo "Uninstallation of Hyper"
   echo "Not implemented"
   exit 1
-  echo "Uninstallation of Hugo complete"
+  echo "Uninstallation of Hyper complete"
 fi
