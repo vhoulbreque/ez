39a14383aba258ca16e560327fd6622e08e7babc
vinzeebreak <vincenthoulbreque@gmail.com>
2018-08-31 10:13:23 +0200

    added install and uninstall of erlang on macos

--- scripts/erlang.sh
+++ scripts/erlang.sh
@@ -2,10 +2,12 @@ platform=$1  # linux-gnu, darwin*
 mode=$2  # install, uninstall
 
 PACKAGE="Erlang"
+PACKAGE_DESCRIPTION="Erlang Programming Language"
 PACKAGE_URL="https://www.erlang.org/"
 
 if [[ $mode == "install" ]]; then
   echo "Installation of $PACKAGE"
+  echo "$PACKAGE_DESCRIPTION"
   echo "$PACKAGE_URL"
 
   if [[ $platform == "linux-gnu" ]]; then
@@ -21,15 +23,21 @@ if [[ $mode == "install" ]]; then
     rm -rf erlang-solutions_1.0_all.deb
 
   elif [[ $platform == "darwin"* ]]; then
-    echo "Not currently implemented"
-    exit 1
+    brew install erlang
   fi
 
   echo "Installation of $PACKAGE complete"
 
 elif [[ $mode == "uninstall" ]]; then
   echo "Uninstallation of $PACKAGE"
-  echo "Not implemented"
-  exit 1
+
+  if [[ $platform == "linux-gnu" ]]; then
+    echo "Not implemented"
+    exit 1
+
+  elif [[ $platform == "darwin"* ]]; then
+    brew uninstall erlang
+  fi
+
   echo "Uninstallation of $PACKAGE complete"
 fi
