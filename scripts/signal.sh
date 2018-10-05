65b71edd3883ea2dd0b381a6d8981c28ac32317a
vinzeebreak <vincenthoulbreque@gmail.com>
2018-10-05 10:14:09 +0200

    added signal install for linux and macos

---
+++ scripts/signal.sh
@@ -0,0 +1,31 @@
+platform=$1  # linux-gnu, darwin*
+mode=$2  # install, uninstall
+
+PACKAGE="Boost"
+PACKAGE_URL="https://www.boost.org/"
+
+if [[ $mode == "install" ]]; then
+  echo "Installation of $PACKAGE"
+  echo "$PACKAGE_URL"
+
+  if [[ $platform == "linux-gnu" ]]; then
+    sudo echo ""
+
+    curl -s https://updates.signal.org/desktop/apt/keys.asc | sudo apt-key add -
+    echo "deb [arch=amd64] https://updates.signal.org/desktop/apt xenial main" | sudo tee -a /etc/apt/sources.list.d/signal-xenial.list
+    sudo apt update && sudo apt install signal-desktop
+  elif [[ $platform == "darwin"* ]]; then
+    sudo echo ""
+
+    cd ~/Downloads
+    brew cask signal
+  fi
+
+  echo "Installation of $PACKAGE complete"
+
+elif [[ $mode == "uninstall" ]]; then
+  echo "Uninstallation of $PACKAGE"
+  echo "Not implemented"
+  exit 1
+  echo "Uninstallation of $PACKAGE complete"
+fi
