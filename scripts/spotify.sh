bb65a7aa62240e077faf1b7e0ec2c0d913f20075
vinzeebreak <vincenthoulbreque@gmail.com>
2018-10-11 10:57:56 +0200

    added (un)install of spotify on macos

--- scripts/spotify.sh
+++ scripts/spotify.sh
@@ -15,15 +15,19 @@ if [[ $mode == "install" ]]; then
     sudo apt-get install spotify-client -y
 
   elif [[ $platform == "darwin"* ]]; then
-    echo "Not implemented"
-    exit 1
+    brew cask install spotify
   fi
 
   echo "Installation of $PACKAGE complete"
 
 elif [[ $mode == "uninstall" ]]; then
   echo "Uninstallation of $PACKAGE"
-  echo "Not implemented"
-  exit 1
+
+  if [[ $platform == "linux-gnu" ]]; then
+    echo "Not implemented"
+    exit 1
+  elif [[ $platform == "darwin"* ]]; then
+      brew cask uninstall spotify
+  fi
   echo "Uninstallation of $PACKAGE complete"
 fi
